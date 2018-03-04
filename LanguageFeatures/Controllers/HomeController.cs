3d6046a20229f572f5400d5abbc5661fc531794d
Maciej Krebs <maciej.krebs@gmail.com>
2018-03-04 09:40:00 +0100

    Nic ciekawego

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Przejście do adresu URL pokazującego przykład";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+
+            myProduct.Name = "Kajak";
+
+            string productName = myProduct.Name;
+
+            return View("Result", (object)String.Format("Nazwa produktu: {0}", productName));
+        }
+    }
+}
