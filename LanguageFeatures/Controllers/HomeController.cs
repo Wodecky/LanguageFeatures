06e9e461fa925f4eb60824b8a596a434087d628f
Maciej Krebs <maciej.krebs@gmail.com>
2018-03-04 13:58:52 +0100

    Koniec

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -25,5 +25,78 @@ namespace LanguageFeatures.Controllers
 
             return View("Result", (object)String.Format("Nazwa produktu: {0}", productName));
         }
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product()
+            {
+                ProductID = 100,
+                Name = "Kajak",
+                Description = "Łódka jednoosobowa",
+                Price = 275M,
+                Category = "Sporty wodne"
+            };
+
+            return View("Result", (object)String.Format($"Kategoria: {myProduct.Category}"));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product{Name="Kajak", Price=275M },
+                    new Product{Name="Kamizelka ratunkowa", Price= 48.95M},
+                    new Product{Name="Piłka nożna", Price= 19.50M},
+                    new Product{Name="Flaga narożna", Price= 34.95M}
+                }
+            };
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result", (object)String.Format($"Razem {cartTotal:c}"));
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product{Name="Kajak", Category="Sporty wodne", Price=275M },
+                    new Product{Name="Kamizelka ratunkowa", Category="Sporty wodne", Price= 48.95M},
+                    new Product{Name="Piłka nożna", Category="Piłka nożna", Price= 19.50M},
+                    new Product{Name="Flaga narożna", Category="Piłka nożna", Price= 34.95M}
+                }
+            };
+
+            decimal total = 0;
+            foreach (Product prod in products.FilterByCategory("Piłka nożna"))
+            {
+                total += prod.Price;
+            }
+            return View("Result", (object)String.Format($"Razem koszyk: {total}"));
+        }
+
+        public ViewResult UseFilterExtensionethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product{Name="Kajak", Category="Sporty wodne", Price=275M },
+                    new Product{Name="Kamizelka ratunkowa", Category="Sporty wodne", Price= 48.95M},
+                    new Product{Name="Piłka nożna", Category="Piłka nożna", Price= 19.50M},
+                    new Product{Name="Flaga narożna", Category="Piłka nożna", Price= 34.95M}
+                }
+            };
+
+            decimal total = 0;
+            foreach (Product prod in products.Filter(prod => prod.Category == "Piłka nożna" || prod.Price>20))
+            {
+                total += prod.Price;
+            }
+            return View("Result", (object)String.Format($"Razem koszyk: {total}"));
+        }
     }
 }
--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -9,10 +9,20 @@ namespace LanguageFeatures.Models
     {
         private string name;
 
+        public int ProductID { get; set; }
         public string Name
         {
-            get { return name; }
-            set { name = value; }
+            get
+            {
+                return ProductID + name;
+            }
+            set
+            {
+                name = value;
+            }
         }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
